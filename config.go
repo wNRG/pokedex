a51067535e9bc723c34a6d2b2bf9eeb0004fb019
wNRG <galamaywindsor@gmail.com>
2026-09-04 15:43:41 +0800

    refactor: added getCommands()

--- config.go
+++ config.go
@@ -4,3 +4,19 @@ type config struct {
 	commands map[string]cliCommand
 } 
 
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+
+		"exit": {
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+		},
+	}
+}
+
--- main.go
+++ main.go
@@ -1,21 +1,9 @@
 package main
 
 func main() {
-	cfg := config{
-		commands: map[string]cliCommand{
-			"help": {
-				name: "help",
-				description: "Displays a help message",
-				callback: commandHelp,
-			},
-
-			"exit": {
-				name: "exit",
-				description: "Exit the Pokedex",
-				callback: commandExit,
-			},
-		},
+	cfg := &config{
+		commands: getCommands(),
 	}
-	startRepl(&cfg)
+	startRepl(cfg)
 }
 
