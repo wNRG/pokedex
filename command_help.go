cfa58b4a2fe82faa8c0aca197bc9ae6697e8b9da
wNRG <galamaywindsor@gmail.com>
2026-09-06 10:00:47 +0800

    added constant order for list of commands in help

--- command_help.go
+++ command_help.go
@@ -4,13 +4,30 @@ import (
 	"fmt"
 )
 
+var order = []string{
+	"help",
+	"map",
+	"mapb",
+	"explore",
+	"catch",
+	"inspect",
+	"pokedex",
+	"exit",
+}
+
 func commandHelp(cfg *config, args ...string) error {
 	fmt.Println("\nWelcome to the Pokedex!")
 	fmt.Println("Usage: ")
 	fmt.Println()
-	for _,cmd := range cfg.commands {
-		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+
+	commands := cfg.commands
+
+	for _, name := range order {
+		if cmd, exists := commands[name]; exists {
+			fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+		}
 	}
+
 	fmt.Println()
 	return nil
 }
